4d7167ae05670c2970191c6040140971b944c1e4
DESKTOP-9C5V6IG\Mai Lý <ngothimaily1510@gmail.com>
2018-01-11 20:08:45 +0700

    Update QL khách hàng và loại khách hàng

--- SouceCode/Hotel/src/main/java/com/httt/controller/LoaiKhachHangController.java
+++ SouceCode/Hotel/src/main/java/com/httt/controller/LoaiKhachHangController.java
@@ -1,9 +1,18 @@
 package com.httt.controller;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.httt.model.LoaiKhachHang;
 import com.httt.service.LoaiKhachHangService;
 
 @Controller
@@ -13,7 +22,46 @@ public class LoaiKhachHangController {
 	@Autowired
 	private LoaiKhachHangService loaikhachhangService;
 	
-
+	@GetMapping({"/loaikhachhang","/"})
+	public String index(Model model) 
+	{
+		model.addAttribute("loaikhachhangs", loaikhachhangService.findAll());
+		return "loaikhachhang";
+	}
+	
+	@GetMapping("/loaikhachhang/create")
+	public String create(Model model) {
+		
+		model.addAttribute("loaikhachhang", new LoaiKhachHang());
+		return "themLoaiKhachHang";
+	}
+	
+	@GetMapping("/loaikhachhang/{id}/edit")
+	public String edit(@PathVariable int id, Model model) {
+		model.addAttribute("loaikhachhang", loaikhachhangService.findOne(id));
+		return "suaLoaiKhachHang";
+	}
+	
+	@GetMapping("/loaikhachhang/{id}/delete")
+	public String delete(@PathVariable int id, RedirectAttributes redirect) {
+		loaikhachhangService.delete(id);
+		redirect.addFlashAttribute("success", "Xóa loại khách hàng thành công!");
+		return "redirect:/loaikhachhang";
+	}
+	
+	
+	@PostMapping("/loaikhachhang/save")
+	public String save(@Valid LoaiKhachHang loaikhachhang, BindingResult result, RedirectAttributes redirect) {
+		if (result.hasErrors()) {
+			redirect.addFlashAttribute("failed", "Thêm loại khách hàng không thành công, vui lòng thử lại!");
+			return "themLoaiKhachHang";
+		}
+		
+		
+		loaikhachhangService.save(loaikhachhang);
+		redirect.addFlashAttribute("success", "Lưu loại khách hàng thành công!");
+		return "redirect:/loaikhachhang";
+	}
 	
 }
 
